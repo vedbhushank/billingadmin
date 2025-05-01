3415fa6e8fa6462ee47e184f6fdc98fc3c739330
Ved Bhushan <digitalvedaa@gmail.com>
2025-05-01 14:31:12 +0530

    code updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,6 @@
     <link href="{{asset('assets')}}/css/app.min.css" rel="stylesheet" type="text/css" />
     <!-- custom Css-->
     <link href="{{asset('assets')}}/css/custom.min.css" rel="stylesheet" type="text/css" />
-
 </head>
 
 <body>
